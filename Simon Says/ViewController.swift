12ebbf1d97839a1ea1a92bcc80ed54bb5b034062
Eddie Hu <ehu23@users.noreply.github.com>
2018-09-08 18:56:51 -0700

    Multiplayer complete with game restart ability and score counter

--- Simon Says/ViewController.swift
+++ Simon Says/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
     var colorSequence = [Int]()
     var colorsToTap = [Int]()
     
+    var gameEnded = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         colorButtons = colorButtons.sorted() {
@@ -40,6 +42,13 @@ class ViewController: UIViewController {
         
         createNewGame()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if gameEnded {
+            gameEnded = false
+            createNewGame()
+        }
+    }
 
     func createNewGame() {
         colorSequence.removeAll()
@@ -50,6 +59,24 @@ class ViewController: UIViewController {
             button.alpha = 0.5
             button.isEnabled = false
         }
+        
+        currentPlayer = 0
+        scores = [0,0]
+        playerLabels[currentPlayer].alpha = 1.0
+        playerLabels[1].alpha = 0.75
+        updateScoreLabels()
+    }
+    
+    func updateScoreLabels() {
+        for (index,label) in scoreLabels.enumerated() {
+            label.text = "\(scores[index])"
+        }
+    }
+    
+    func switchPlayers() {
+        playerLabels[currentPlayer].alpha = 0.75
+        currentPlayer = currentPlayer == 0 ? 1 : 0
+        playerLabels[currentPlayer].alpha = 1.0
     }
     
     func addNewColor() {
@@ -79,6 +106,12 @@ class ViewController: UIViewController {
         }
     }
     
+    func endGame() {
+        let message = currentPlayer == 0 ? "Player 2 Wins!" : "Player 1 Wins!"
+        actionButton.setTitle(message, for: .normal)
+        gameEnded = true
+    }
+    
     @IBAction func colorButtonHandler(_ sender: CircularButton) {
         
         if sender.tag == colorsToTap.removeFirst() {
@@ -87,6 +120,7 @@ class ViewController: UIViewController {
             for button in colorButtons {
                 button.isEnabled = false
             }
+            endGame()
             return
         }
         
@@ -94,6 +128,9 @@ class ViewController: UIViewController {
             for button in colorButtons {
                 button.isEnabled = false
             }
+            scores[currentPlayer] += 1
+            updateScoreLabels()
+            switchPlayers()
             actionButton.setTitle("Continue", for: .normal)
             actionButton.isEnabled = true
         }
