2adebd90ffea5a380b7930c970e162c2d9d76757
Eddie Hu <ehu23@users.noreply.github.com>
2018-09-08 18:42:05 -0700

    Basic single player functionality works without score or restart game settings

--- Simon Says/ViewController.swift
+++ Simon Says/ViewController.swift
@@ -10,16 +10,109 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var colorButtons: [CircularButton]!
+    
+    @IBOutlet weak var actionButton: UIButton!
+    
+    @IBOutlet var playerLabels: [UILabel]!
+    
+    @IBOutlet var scoreLabels: [UILabel]!
+    
+    var currentPlayer = 0
+    var scores = [0,0]
+    
+    var sequenceIndex = 0
+    var colorSequence = [Int]()
+    var colorsToTap = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        colorButtons = colorButtons.sorted() {
+            $0.tag < $1.tag
+        }
+        playerLabels = playerLabels.sorted() {
+            $0.tag < $1.tag
+        }
+        scoreLabels = scoreLabels.sorted() {
+            $0.tag < $1.tag
+        }
+        
+        createNewGame()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createNewGame() {
+        colorSequence.removeAll()
+        
+        actionButton.setTitle("Start Game", for: .normal)
+        actionButton.isEnabled = true
+        for button in colorButtons {
+            button.alpha = 0.5
+            button.isEnabled = false
+        }
     }
-
+    
+    func addNewColor() {
+        colorSequence.append(Int(arc4random_uniform(UInt32(4))))
+    }
+    
+    func playSequence() {
+        if sequenceIndex < colorSequence.count {
+            flash(button: colorButtons[colorSequence[sequenceIndex]])
+            sequenceIndex += 1
+        } else {
+            colorsToTap = colorSequence
+            view.isUserInteractionEnabled = true
+            actionButton.setTitle("Tap Tap Tap", for: .normal)
+            for button in colorButtons {
+                button.isEnabled = true
+            }
+        }
+    }
+    
+    func flash(button: CircularButton) {
+        UIView.animate(withDuration: 0.5, animations: {
+            button.alpha = 1.0
+            button.alpha = 0.5
+        }) { (bool) in
+            self.playSequence()
+        }
+    }
+    
+    @IBAction func colorButtonHandler(_ sender: CircularButton) {
+        
+        if sender.tag == colorsToTap.removeFirst() {
+            
+        } else {
+            for button in colorButtons {
+                button.isEnabled = false
+            }
+            return
+        }
+        
+        if colorsToTap.isEmpty {
+            for button in colorButtons {
+                button.isEnabled = false
+            }
+            actionButton.setTitle("Continue", for: .normal)
+            actionButton.isEnabled = true
+        }
+        
+    }
+    
+    @IBAction func actionButtonHandler(_ sender: UIButton) {
+        
+        sequenceIndex = 0
+        actionButton.setTitle("Memorize It", for: .normal)
+        actionButton.isEnabled = false
+        view.isUserInteractionEnabled = false
+        addNewColor()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            self.playSequence()
+        }
+        
+    }
+    
 
 }
 
